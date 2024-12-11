0a5af3a8cc0cf4e81f07b7a633de12c1ca0dfd66
Mauricio Chacon <mauriciochaconmartinez05@gmail.com>
2024-12-11 00:36:38 -0500

    feat: add global site configuration and type definitions

---
+++ src/types.ts
@@ -0,0 +1,20 @@
+export type Page = {
+  title: string;
+  description: string;
+};
+
+export interface Site extends Page {
+  author: string;
+}
+
+export type Links = {
+  text: string;
+  href: string;
+}[];
+
+export type Socials = {
+  name: string;
+  icon: string;
+  text: string;
+  href: string;
+}[];
