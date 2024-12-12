f7312d543d13a864383902f0e029f35310413643
Mauricio Chacon <mauriciochaconmartinez05@gmail.com>
2024-12-11 22:45:19 -0500

    feat: add utility function for class name merging

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ import type { Site, Page, Links, Socials } from "@types";
 
 // Global
 export const SITE: Site = {
-  title: "Academia Deportiva Cantolao Chorrillos",
+  title: "Cantolao Chorrillos",
   description: `¡Ven y forma parte de la Academia Deportiva Cantolao Sede Chorrillos! ¿A tu hijo le gusta el fútbol?, ayúdalo a desarrollar todo su potencial… ⚽️⚽️🥅 Nos especializamos en la formación de niños desde los 3 años, ofreciendo fútbol a medida, que incluye preparación física, técnica y táctica. Nuestros entrenadores altamente capacitados se aseguran de que tu hijo aprenda las habilidades esenciales para el éxito en el campo de juego, mientras se divierte y desarrolla su pasión por el deporte.¡Únete a nuestra gran familia deportiva hoy mismo y descubre por qué Cantolao Sede Chorrillos es la elección perfecta para tu hijo! ¡Contáctanos para más información!`,
   author: "Mauricio Chacon Martinez",
 };
@@ -15,26 +15,22 @@ export const LINKS: Links = [
     text: "Inicio",
     href: "/",
   },
+  {
+    text: "Prueba 1",
+    href: "/",
+  },
+  {
+    text: "Prueba 2",
+    href: "/",
+  },
+  {
+    text: "Prueba 3",
+    href: "/",
+  },
+  {
+    text: "Prueba 4",
+    href: "/",
+  },
 ];
 
-// Socials
-export const SOCIALS: Socials = [
-  {
-    name: "Facebook",
-    icon: "facebook",
-    text: "Facebook - Cantolao Chorrillos",
-    href: "https://www.facebook.com/CantolaoChorrilloss/?locale=es_LA",
-  },
-  {
-    name: "Youtube",
-    icon: "youtube",
-    text: "Youtube - Cantolao Chorrillos",
-    href: "https://www.youtube.com/@cantolaochorrillos",
-  },
-  {
-    name: "Instagram",
-    icon: "instagram",
-    text: "Instagram - Cantolao Chorrillos",
-    href: "https://www.instagram.com/cantolao.chorrillos/",
-  },
-];
+
