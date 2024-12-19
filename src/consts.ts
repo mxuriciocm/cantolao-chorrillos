f7eed0097683e86b165c2e49d6267633bca95928
Mauricio Chacon Martinez <mauriciochaconmartinez05@gmail.com>
2024-12-18 23:26:51 -0500

    feat: add Footer component for enhanced site navigation and branding; update Header component size and site title

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ import type { Site, Page, Links, Socials, Stats, Features } from "@types";
 
 // Global
 export const SITE: Site = {
-  title: "Cantolao",
+  title: "Cantolao Chorrillos",
   description: `¡Ven y forma parte de la Academia Deportiva Cantolao Sede Chorrillos! ¿A tu hijo le gusta el fútbol?, ayúdalo a desarrollar todo su potencial… ⚽️⚽️🥅 Nos especializamos en la formación de niños desde los 3 años, ofreciendo fútbol a medida, que incluye preparación física, técnica y táctica. Nuestros entrenadores altamente capacitados se aseguran de que tu hijo aprenda las habilidades esenciales para el éxito en el campo de juego, mientras se divierte y desarrolla su pasión por el deporte.¡Únete a nuestra gran familia deportiva hoy mismo y descubre por qué Cantolao Sede Chorrillos es la elección perfecta para tu hijo! ¡Contáctanos para más información!`,
   author: "Mauricio Chacon Martinez",
 };
